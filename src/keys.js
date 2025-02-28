e350cfeef54b5e9122c4bcfb3c1c96ae12fa1eae
CF-Soft <149086025+CF-Soft@users.noreply.github.com>
2025-02-28 22:09:34 +0400

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 export const keys = {
-  api: "http://46.202.153.253:3000/api/v1",
+  api: "http://46.202.153.253:3001/api/v1",
   token: JSON.parse(localStorage.getItem("token")),
   email: JSON.parse(localStorage.getItem("email")),
 };
